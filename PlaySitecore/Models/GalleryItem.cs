2d778f8cb383d6d4bfbb8165cad015e364d41639
HOME\Coolio <jkerny81@gmail.com>
2017-10-22 13:30:09 +1100

    Add id to fix experience editor bug

--- PlaySitecore/Models/GalleryItem.cs
+++ PlaySitecore/Models/GalleryItem.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Glass.Mapper.Sc.Fields;
+using Glass.Mapper.Sc.Configuration.Attributes;
 
 namespace PlaySitecore.Models
 {
     public interface IGalleryItem
     {
+        [SitecoreId]
+        Guid Id { get; set; }
         string Title { get; set; }
         string Description { get; set; }
         Image Image { get; set; }
