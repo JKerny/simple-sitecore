ec52b36a439811cea92b89768cd5ec7daff1496e
HOME\Coolio <jkerny81@gmail.com>
2017-10-22 12:19:20 +1100

    Adding a homepage gallery plus all items serialized items from sitecore

---
+++ PlaySitecore/Models/GalleryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sc.Fields;
+
+namespace PlaySitecore.Models
+{
+    public interface IGalleryItem
+    {
+        string Title { get; set; }
+        string Description { get; set; }
+        Image Image { get; set; }
+
+    }
+}
--- PlaySitecore/Models/Home.cs
+++ PlaySitecore/Models/Home.cs
@@ -10,5 +10,7 @@ namespace PlaySitecore.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+        public IEnumerable<IGalleryItem> GalleryItems { get; set; }
+
     }
 }
